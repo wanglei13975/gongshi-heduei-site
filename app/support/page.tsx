39bbcb93bc443f1bfb9491c2201a9af88c6a215d
wanglei <wanglei@13975.com>
2026-09-02 12:30:31 +0800

    Route remaining CTAs through wage-check custom page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-html-link-for-pages -- GitHub Pages static export needs plain anchors with the repository base path. */
-const appStoreURL = "https://apps.apple.com/cn/app/%E5%B7%A5%E6%97%B6%E6%A0%B8%E5%AF%B9/id6800185417?ct=github_support&mt=8";
+const appStoreURL = "https://apps.apple.com/cn/app/%E5%B7%A5%E6%97%B6%E6%A0%B8%E5%AF%B9/id6800185417?ppid=2a335c72-49d1-4510-9067-1c9c78518572&ct=github_support&mt=8";
 
 export default function Support() {
   return <main className="legal">
